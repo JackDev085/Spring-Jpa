6daa33d0c801b9a0d4ed66f2ae7837b7fff4fcef
Jackson Lorran <computadordb@gmail.com>
2023-06-19 08:46:46 -0300

    User entity and resource

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,8 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Anotação principal para dizer onde o programa começa
+
 @SpringBootApplication
 public class DemoApplication {
 
